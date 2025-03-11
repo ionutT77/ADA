e92eeedcb6230daa8a0d2c7630696e1ff9eef2ea
ionutT77 <ionut7797@gmail.com>
2025-03-11 13:08:25 +0200

    lab2_ada

--- lab2_updated_ada/src/Person.java
+++ lab2_updated_ada/src/Person.java
@@ -1,7 +1,7 @@
 public class Person implements Comparable<Person> {
-    private String firstName;
-    private String lastName;
-    private int age;
+    private final String firstName;
+    private final String lastName;
+    private final int age;
 
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
